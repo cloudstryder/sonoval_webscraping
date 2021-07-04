60dd5efdcb6285f8ab04635f28dbf0cb4a21a7a8
cloudstryder <rohinsshanker@gmail.com>
2021-07-03 19:22:33 -0700

    updated some comments

--- sonoval_webscraping.py
+++ sonoval_webscraping.py
@@ -15,7 +15,7 @@ from requests_html import HTMLSession
 workbook = load_workbook(filename='research_papers_list.xlsx') 
 sheet = workbook.active  
 session = HTMLSession() 
-# index to search for emails. it still picks up some data tags or long strings starting with // which can be easily filtered out later, but I forgot to add that
+# index to search for emails. it still picks up some data tags or long strings starting with // which i should filter out the next time
 EMAIL_REGEX = r"""(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])"""
 
 # loads webdriver, needs to be installed, and the final breadcrumb of the filepath should be the actual webdriver
